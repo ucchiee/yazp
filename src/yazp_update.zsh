d4d93e26cab0b48039e9391267faa3f4738a457d
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-07-10 20:17:48 +0900

    yazp_update.zsh: debug, update command

--- src/yazp_update.zsh
+++ src/yazp_update.zsh
@@ -1,6 +1,6 @@
 # shellcheck shell=bash
 function yazp_update() {
-    current_dir="$(dirname "$0")"
+    current_dir="$(pwd)"
     cd "$YAZPDIR" || exit
     git pull
     cd "$current_dir" || exit
