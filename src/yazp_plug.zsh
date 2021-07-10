d4e3533bf29eea1c1d21a1bdd4df1e9548fb659f
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-07-10 19:31:30 +0900

    yazp_plug.zsh: double quoted

--- src/yazp_plug.zsh
+++ src/yazp_plug.zsh
@@ -13,9 +13,9 @@ function yazp_add_plugin() {
     # $2: Specified file name to source
     PLUGIN_NAME=$(echo "$1" | cut -d "/" -f 2)
     if [ -d "$YAZPDIR/plugins/$PLUGIN_NAME" ]; then
-        zsh_enable_plugin "$PLUGIN_NAME" $2
+        zsh_enable_plugin "$PLUGIN_NAME" "$2"
     else
         git clone "https://github.com/$1.git" "$YAZPDIR/plugins/$PLUGIN_NAME"
-        zsh_enable_plugin "$PLUGIN_NAME" $2
+        zsh_enable_plugin "$PLUGIN_NAME" "$2"
     fi
 }
