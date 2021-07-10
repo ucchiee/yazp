df5bb7dde998a5f744f6bd104b3b624876bb87b9
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-07-10 19:22:01 +0900

    yazp_plug.zsh: additional file to source

--- src/yazp_plug.zsh
+++ src/yazp_plug.zsh
@@ -2,18 +2,20 @@
 
 function zsh_enable_plugin() {
     # $1: plugin name
+    # $2: specified file name to source
     yazp_add_file "plugins/$1/$1.plugin.zsh" ||
         yazp_add_file "plugins/$1/$1.zsh" ||
-        yazp_add_file "plugins/$1/$1.sh"
+        yazp_add_file "plugins/$1/$2"
 }
 
 function yazp_add_plugin() {
     # $1: username/plugin_name
+    # $2: Specified file name to source
     PLUGIN_NAME=$(echo "$1" | cut -d "/" -f 2)
     if [ -d "$YAZPDIR/plugins/$PLUGIN_NAME" ]; then
-        zsh_enable_plugin "$PLUGIN_NAME"
+        zsh_enable_plugin "$PLUGIN_NAME" $2
     else
         git clone "https://github.com/$1.git" "$YAZPDIR/plugins/$PLUGIN_NAME"
-        zsh_enable_plugin "$PLUGIN_NAME"
+        zsh_enable_plugin "$PLUGIN_NAME" $2
     fi
 }
