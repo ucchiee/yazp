f127ec89050c18621d4e784ac5c5a48a695ca9aa
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-07-15 11:29:19 +0900

    yazp_upgrade.zsh: formatted

--- src/yazp_upgrade.zsh
+++ src/yazp_upgrade.zsh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 # shellcheck disable=SC2164
 function yazp_upgrade() {
-    if [ ! -e $YAZPDIR ]; then
+    if [ ! -e "$YAZPDIR" ]; then
         echo "$YAZPDIR was not found"
         return
     fi
