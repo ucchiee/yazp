b03b50d3b60e30bd7fa5c3d521f6ebb6f86b6723
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-07-15 10:30:25 +0900

    yazp_upgrade.zsh: add error checking

--- src/yazp_upgrade.zsh
+++ src/yazp_upgrade.zsh
@@ -1,8 +1,12 @@
 # shellcheck shell=bash
 # shellcheck disable=SC2164
 function yazp_upgrade() {
+    if [ ! -e $YAZPDIR ]; then
+        echo "$YAZPDIR was not found"
+        return
+    fi
     current_dir="$(pwd)"
-    cd "$YAZPDIR" || echo "$YAZPDIR was not found" && return
+    cd "$YAZPDIR"
     git pull || cd "$current_dir"
     source "$YAZPDIR/yazp.zsh"
     cd "$current_dir"
